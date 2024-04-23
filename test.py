aac8a180a645ea2efccfa3fd67327f7d90755e53
Moonite <moonite1209@qq.com>
2024-04-23 14:26:35 +0800

    clip

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from PIL import Image
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
-image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
-text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
+image = preprocess(Image.open("room.jpg")).unsqueeze(0).to(device)
+text = clip.tokenize(["a diagram", "a dog", "a picture", "picture"]).to(device)
 
 with torch.no_grad():
     image_features = model.encode_image(image)
